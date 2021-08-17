c2236a86480b652cd9f0d6b58cf33c83ecaee952
T-N9 <tenyainmoelwin@gmail.com>
2021-08-17 16:45:55 +0630

    [tn] first commit

---
+++ resources/js/app.js
@@ -0,0 +1,86 @@
+const desktopMenuBtn= document.getElementById('menu-toggleBtn-desktop');
+const mobileMenuBtn= document.getElementById('menu-toggleBtn-mobile');
+
+const navOverlay= document.querySelector('.nav-overlay');
+// Desktop Icons
+const maximize=document.querySelector('.maximize-icon');
+const lg_menuNav=document.querySelector('.lg-side-nav-icon');
+
+const mainSection=document.querySelector('.main');
+const nav=document.querySelector('.nav');
+const sni=document.querySelectorAll('.sni');
+const mi=document.querySelectorAll('.mi');
+
+// Maximize to Side Nav
+function max_to_sn(){
+    var i;
+    maximize.classList.add('maximize-off');
+    maximize.classList.remove('maximize-on');
+
+    maximize.classList.remove('side-nav-off');
+
+    mainSection.classList.add('full-main');
+    nav.classList.add('nav-off');
+
+    for (i = 0; i < sni.length; i++) {
+        sni[i].style.display = "inline-block";
+    }
+    setTimeout(() => {
+        for (i = 0; i < mi.length; i++) {
+            mi[i].style.display = "none";
+        }
+        maximize.classList.add('side-nav-on');
+    }, 500);
+
+    desktopMenuBtn.classList.remove('toggle-max');
+    desktopMenuBtn.classList.add('toggle-sn');
+}
+
+function sn_to_max(){
+    var i;
+    maximize.classList.add('side-nav-off');
+    maximize.classList.remove('side-nav-on');
+
+    maximize.classList.remove('maximize-off');
+    
+    mainSection.classList.remove('full-main');
+    nav.classList.remove('nav-off');
+
+    for (i = 0; i < mi.length; i++) {
+        mi[i].style.display = "inline-block";
+    }
+    setTimeout(() => {
+        for (i = 0; i < sni.length; i++) {
+            sni[i].style.display = "none";
+        }
+        maximize.classList.add('maximize-on');
+    }, 500);
+
+    desktopMenuBtn.classList.remove('toggle-sn');
+    desktopMenuBtn.classList.add('toggle-max');
+}
+
+desktopMenuBtn.onclick=function(){
+
+    if(this.classList.contains('toggle-max')){
+        max_to_sn();
+    }
+    else{
+        sn_to_max();
+    }
+}
+
+mobileMenuBtn.onclick=function(){
+    nav.classList.toggle('x-full-scale');
+    navOverlay.classList.toggle('open-overlay');
+}
+
+navOverlay.onclick=function(){
+    if(nav.classList.contains('x-full-scale')){
+        nav.classList.remove('x-full-scale');
+        this.classList.remove('open-overlay');
+    }
+}
+
+
+
