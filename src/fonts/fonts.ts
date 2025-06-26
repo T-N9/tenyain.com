170b33f7ba0dd855b9e1567a7b40bae360dd229b
T-N9 <thethoriumtension@gmail.com>
2025-06-26 21:52:56 +0700

    💄 Font changed

--- src/fonts/fonts.ts
+++ src/fonts/fonts.ts
@@ -1,5 +1,5 @@
-import {Tektur, JetBrains_Mono} from "next/font/google";
+import {Tektur, Ubuntu_Mono, } from "next/font/google";
 
 
-export const primary_font = JetBrains_Mono({ weight: ['400', '700'], subsets: ['latin'], fallback : ["MyanmarSanpya"] });
+export const primary_font = Ubuntu_Mono({ weight: ['400', '700'], subsets: ['latin'], fallback : ["MyanmarSanpya"] });
 export const heading_font = Tektur({ weight : [ "400" , "500" , "700" , "600"],subsets: ['latin']});
