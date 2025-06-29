bd6f57677391f3fd3f9ba4377459d61511069c2e
T-N9 <thethoriumtension@gmail.com>
2025-06-29 20:35:55 +0700

    🐛 Shhhh leaks fix

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDYEERV_9kQQ5-fcEW8E6qVj7wJ6GyiHtM",
-  authDomain: "tenyain-com.firebaseapp.com",
-  projectId: "tenyain-com",
-  storageBucket: "tenyain-com.firebasestorage.app",
-  messagingSenderId: "877750356699",
-  appId: "1:877750356699:web:0b78c04fc725858f5fdafe"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
 };
 
 const app = initializeApp(firebaseConfig);
